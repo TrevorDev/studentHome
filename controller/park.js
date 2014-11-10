8ffece1054b5b8982b61983115de2af55df0abd7
unknown <trevorjbaron@gmail.com>
2014-11-09 19:37:36 -0500

    parks cntrlr

--- controller/park.js
+++ controller/park.js
@@ -1,12 +1,17 @@
+var database = require('./../lib/database');
+var map = require('./../lib/googleMaps');
+var si = database.getSequelizeInstance();
+
 exports.getPark = function*(){
-	this.jsonResp(200, {
-			results: [{
-				name: "worst park",
-				bball: "yes"
-			},
-			{
-				name: "worst school",
-				bball: "blah"
-			}]
-		})
+	var address = this.params['number']+' '+this.params['street']
+	var latLong = yield map.getLatLong(address);
+	//console.log(latLong)
+	var parks = yield si.query("select *, " +
+		" ( 3959 * acos( cos( radians("+latLong.lat+") ) " +
+		" * cos( radians( parks.Latitude ) ) " +
+		" * cos( radians( parks.Longitude ) - radians("+latLong.lng+") ) " +
+		" + sin( radians("+latLong.lat+") ) " +
+		" * sin( radians( parks.Latitude ) ) ) ) AS distance " +
+		"from parks order by distance asc limit 10")
+	this.jsonResp(200, parks)
 }
